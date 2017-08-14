77bd22c4cb978fbdd374e71da2ac7f4a068d9366
TarcisioVitor <tarcisiosouzabr@gmail.com>
2017-08-14 20:43:40 -0300

    Start Project

---
+++ ArcTouch.TestDevelopment/ArcTouch.TestDevelopment.Service/APIHelper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArcTouch.TestDevelopment.Service
+{
+    public static class APIHelper
+    {
+        private const string apiAddress = "https://api.themoviedb.org/3";
+    
+    }
+}
